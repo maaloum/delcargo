710d20e935f18f9bae22ba90fd8e3157bb3f1605
maaloum <maaloumali1@gmail.com>
2024-10-16 15:26:03 -0400

    Hero

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,18 @@
-import { Container, Navbar, Button } from "react-bootstrap";
+import { Container, Button, Navbar } from "react-bootstrap";
 import "./header.css";
 function header() {
   return (
     <Navbar bg="light" className="mx-5 round-navbar">
       <Container>
+        <div className="sc-fBEEOr aOQZO">
+          <span className="line1"></span>
+          <span className="line2"></span>
+          <span className="hover-line1"></span>
+          <span className="hover-line2"></span>
+        </div>
         <Navbar.Brand href="#home">MyLogo</Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
-          <Button Color="hover-orange">LogIn</Button>
+          <Button className="custom-button px-5 py-2 ">LogIn</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
